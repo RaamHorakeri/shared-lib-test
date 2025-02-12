e75b5819ca24d341f292e22b434cdd0c12fa890e
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2025-02-12 17:13:34 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,7 +1,7 @@
 def call(String appName) {
     script {
         echo "Building Docker image: ${appName.toLowerCase()}"
-        sh "cd reddit-clone-k8s-ingress-main"
+        
         sh "docker build --no-cache -t ${appName.toLowerCase()}:latest ."
     }
 }
