02aa447e108751d5f6cca5b8a42206b82b8b7306
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2025-02-12 15:24:09 +0530

    Create jenkinsPipeline.groovy

---
+++ vars/jenkinsPipeline.groovy
@@ -0,0 +1,74 @@
+@Library('falcon') _
+
+pipeline {
+    agent any
+
+    parameters {
+        choice(name: 'APP_NAME', choices: ['account', 'bifrost', 'community', 'content', 'payment'], description: 'Select Microservice')
+        string(name: 'BRANCH', defaultValue: 'main', description: 'Git branch to checkout')
+        string(name: 'REPO_URL', description: 'Git repository URL')
+        string(name: 'CREDENTIALS_ID', description: 'Jenkins credentials ID')
+    }
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                script {
+                    checkoutFromGit(params.BRANCH, params.REPO_URL, params.CREDENTIALS_ID)
+                }
+            }
+        }
+
+        stage('Load Secrets') {
+            steps {
+                script {
+                    loadSecrets(params.APP_NAME)
+                }
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    buildDockerImage(params.APP_NAME)
+                }
+            }
+        }
+
+        stage('Remove Existing Container') {
+            steps {
+                script {
+                    removeContainer(params.APP_NAME)
+                }
+            }
+        }
+
+        stage('Deploy with Docker Compose') {
+            steps {
+                script {
+                    deployWithDockerCompose()
+                }
+            }
+        }
+
+        stage('Cleanup') {
+            steps {
+                script {
+                    pruneDockerImages()
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            echo 'Deployment complete.'
+        }
+        success {
+            echo 'Deployment succeeded.'
+        }
+        failure {
+            echo 'Deployment failed!'
+        }
+    }
+}
