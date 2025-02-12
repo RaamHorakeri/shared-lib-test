e7d5cb66fbdff68d1146862c488b20c1884b89cf
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2025-02-12 17:15:18 +0530

    Update checkoutFromGit.groovy

--- vars/checkoutFromGit.groovy
+++ vars/checkoutFromGit.groovy
@@ -1,7 +1,24 @@
-def call(String branch, String repoUrl, String credentialsId) {
+// def call(String branch, String repoUrl, String credentialsId) {
+//     checkout([
+//         $class: 'GitSCM',
+//         branches: [[name: "*/${branch}"]],
+//         userRemoteConfigs: [[url: repoUrl, credentialsId: credentialsId]]
+//     ])
+// }
+
+
+// vars/cloneAndNavigate.groovy
+def call(String branch, String repoUrl, String credentialsId, String subDir) {
+    // Checkout the repository with the specified branch and credentials
     checkout([
         $class: 'GitSCM',
         branches: [[name: "*/${branch}"]],
         userRemoteConfigs: [[url: repoUrl, credentialsId: credentialsId]]
     ])
+    
+    // Navigate to the specified sub-directory and run commands
+    dir(subDir) {
+        // Example command to run in the sub-directory
+        sh "echo 'Inside ${subDir} directory'"
+    }
 }
