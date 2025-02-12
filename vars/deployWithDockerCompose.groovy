93341d55a4839ba6f7b7e86e63c24fdbe4f59f9d
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2025-02-12 17:36:33 +0530

    Update deployWithDockerCompose.groovy

--- vars/deployWithDockerCompose.groovy
+++ vars/deployWithDockerCompose.groovy
@@ -2,7 +2,8 @@ def call() {
     script {
         echo "Deploying application with Docker Compose"
         // sh "docker compose up -d"
-        sh "docker compose -f ./reddit-clone-k8s-ingress-main/docker-compose.yml up -d"
+      sh "docker-compose -f ./reddit-clone-k8s-ingress-main/docker-compose.yaml up -d"
+
 
     }
 }
